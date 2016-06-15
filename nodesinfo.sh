32fa350cf5aa7f1a9edb0526acb3f5d2f043927d
kimio_kosaka <kim.kosmac@gmail.com>
2016-06-15 14:27:31 +0900

    Update nodesinfo.sh

--- nodesinfo.sh
+++ nodesinfo.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #set below manualy
 nameprefix=bullet
-netaddr=192.168.10
-routeraddr=$netaddr.253
-dnsaddr=$netaddr.251
+netaddr=192.168.1
+routeraddr=$netaddr.1
+dnsaddr=$netaddr.1
 baseaddr=100
 nodelist='00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31'
